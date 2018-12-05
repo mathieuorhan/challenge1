2134004caac5f6cbdcf3baa497be2d50ddda6afa
Mathieu Orhan <mathieupowa@gmail.com>
2018-12-05 12:06:46 +0100

    added label fnames loader

--- data_loader.py
+++ data_loader.py
@@ -6,8 +6,9 @@ class DataLoader:
 
     def __init__(self, data_path):
         self.data_path = data_path
+        self.load_link()
 
-    def load_link(self, fname):
+    def load_link(self):
         """ Load the link file and parse it
         :param fname: The filename of the link file
 
@@ -16,8 +17,9 @@ class DataLoader:
 
         :return: A dictonary whose keys are the dicoms and values contourfiles for each patient
         """
+        link_fname = os.path.join(self.data_path, 'link.csv')
         self.link_dict = {}
-        with open(fname, mode='r') as infile:
+        with open(link_fname, mode='r') as infile:
             reader = DictReader(infile, delimiter=',')
             for row in reader:
                 self.link_dict[row['patient_id']] = row['original_id']
@@ -25,8 +27,10 @@ class DataLoader:
 
     def load_patient_data(self, patient_id):
         # Get dcm filenames from the corresponding folder
+        dicom_fnames = self.get_patient_dicom_fnames(patient_id)
+        label_fnames = self.get_patient_label_fnames(patient_id)
+        print(label_fnames)
         
-        pass
 
     def get_patient_dicom_fnames(self, patient_id):    
         """ Get the filenames of the DICOM files for a given patient
@@ -37,6 +41,19 @@ class DataLoader:
         """    
         patient_dicom_pattern = os.path.join(self.data_path, 'dicoms', patient_id, '*.dcm')
         fnames = glob(patient_dicom_pattern)
+        fnames = [fname for fname in fnames if not os.path.basename(fname).startswith('.')]
+        return fnames
+    
+    def get_patient_label_fnames(self, patient_id):
+        """ Get the filenames of the label (i-contour) files for a given patient
+
+        :param patient_id: string, the id of the patient
+    
+        :return: The label filenames as a list
+        """    
+        label_id = self.link_dict[patient_id]
+        patient_label_pattern = os.path.join(self.data_path, 'contourfiles', label_id, 'i-contours', '*.txt')
+        fnames = glob(patient_label_pattern)
         fnames = [fname for fname in fnames if not os.path.basename(fname).startswith('.')]
         return fnames
 
